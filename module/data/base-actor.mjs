443aa3bf2a7056b6313e69a672df8b857cd28ec7
SouOWendel <souowendel@gmail.com>
2025-08-24 22:16:07 -0300

    feat: added mana and stamina, removed power

---
+++ module/data/base-actor.mjs
@@ -0,0 +1,26 @@
+export default class arcanaActorBase extends foundry.abstract
+  .TypeDataModel {
+  static LOCALIZATION_PREFIXES = ["ARC.Actor.base"];
+
+  static defineSchema() {
+    const fields = foundry.data.fields;
+    const requiredInteger = { required: true, nullable: false, integer: true };
+    const schema = {};
+
+    schema.health = new fields.SchemaField({
+      value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
+      max: new fields.NumberField({ ...requiredInteger, initial: 10 }),
+    });
+    schema.mana = new fields.SchemaField({
+      value: new fields.NumberField({ ...requiredInteger, initial: 5, min: 0 }),
+      max: new fields.NumberField({ ...requiredInteger, initial: 5 }),
+    });
+		schema.stamina = new fields.SchemaField({
+      value: new fields.NumberField({ ...requiredInteger, initial: 5, min: 0 }),
+      max: new fields.NumberField({ ...requiredInteger, initial: 5 }),
+    });
+    schema.biography = new fields.HTMLField();
+
+    return schema;
+  }
+}
